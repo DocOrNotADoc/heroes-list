084965a362d5b34a1eed9cd5745b338d43be1bb4
Vik <leesunsinn@protonmail.com>
2022-07-16 11:56:02 +0300

    createAsyncThunk

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -4,8 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup} from 'react-transition-group';
 import { createSelector } from 'reselect';
 
-import { fetchHeroes } from '../../actions';
-import { heroDeleted } from './heroesSlice';
+import { fetchHeroes,heroDeleted } from './heroesSlice';
 
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
